e68dd2846818651c2421c741ce17ff2cdf22a0c6
Mindwrought <heyitsanotheremail@gmail.com>
2025-06-28 12:40:29 -0500

    2nd asteroids bootdev commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame 
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -10,6 +11,17 @@ def main():
     window= pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
 
+
+# start of defining variables code
+
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH /2
+    y = SCREEN_HEIGHT /2
+    player = Player(x, y)
+
+#  Start of game loop code
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -17,7 +29,11 @@ def main():
                 running = False
 
         window.fill((0,0,0))
+        player.draw(window)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
+
 
     pygame.quit()
 
